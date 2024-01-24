4bbc03fbdc8512591477326c1e9e45dbfce4e472
Alamy <mestafaalami11@gmail.com>
2024-01-24 01:40:10 +0100

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -9,7 +9,7 @@ file {'/var/www/html/index.nginx-debian.html':
 file_line {'configure redirection':
     path  =>  '/etc/nginx/sites-available/default',
     after =>  'server_name _;',
-    line  =>  "\n\tlocation /redirect_me {\n\t\treturn 301 https://youtu.be/dQw4w9WgXcQ;\n\t}\n",
+    line  =>  "\n\tlocation /redirect_me {\n\t\treturn 301 https://github.com/alamy2711;\n\t}\n",
 }
 service {'nginx':
     ensure => running,
