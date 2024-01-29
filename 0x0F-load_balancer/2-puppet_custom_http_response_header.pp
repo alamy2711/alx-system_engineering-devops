345b89629e0e628a249b70918926181faaed85db
Alamy <mestafaalami11@gmail.com>
2024-01-30 00:44:19 +0100

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,9 +1,9 @@
 # Add a custom HTTP header with Puppet
 
 exec { 'command':
-  command  => 'apt-get -y update;
-  apt-get -y install nginx;
-  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  command  => 'apt-get --yes update;
+  apt-get --yes install nginx;
+  sudo sed --in-place "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
   service nginx restart',
   provider => shell,
 }
